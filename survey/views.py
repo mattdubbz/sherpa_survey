af4db97a550402e5c37f6fbecfa3d90f8bbcecad
mattdubbz <matthew.whisman@gmail.com>
2019-12-07 17:58:09 -0500

    fixed forms by adding FormView and form_class to view along with correct url name to success_url

--- survey/views.py
+++ survey/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 
 from .models import Survey
 from .forms import SurveyForm
 
 # Create your views here.
-class SurveyView(TemplateView):
+class SurveyView(FormView):
     model = Survey
     form = SurveyForm
+    form_class = SurveyForm
     template_name = "survey.html"
-    success_url = reverse_lazy("survey_complete.html")
+    success_url = reverse_lazy("survey_complete")
 
 
 class SurveyComplete(TemplateView):

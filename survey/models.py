f4430f3174dd5d4e3d9f20806df4b3a12384611b
mattdubbz <matthew.whisman@gmail.com>
2019-12-13 15:59:42 -0500

    changed SureyForm structure to include widgets

--- survey/models.py
+++ survey/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
 
 class Survey(models.Model):
-    room_score1 = models.IntegerField(blank=True, null=True)
-    room_score2 = models.IntegerField(blank=True, null=True)
-    room_score3 = models.IntegerField(blank=True, null=True)
-    room_score4 = models.IntegerField(blank=True, null=True)
-    room_score5 = models.IntegerField(blank=True, null=True)
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+    SCORE_CHOICES = [(ONE, "1"), (TWO, "2"), (THREE, "3"), (FOUR, "4"), (FIVE, "5")]
+
+    room_score1 = models.IntegerField(blank=True, null=True, choices=SCORE_CHOICES)
+    room_score2 = models.IntegerField(blank=True, null=True, choices=SCORE_CHOICES)
+    room_score3 = models.IntegerField(blank=True, null=True, choices=SCORE_CHOICES)
+    room_score4 = models.IntegerField(blank=True, null=True, choices=SCORE_CHOICES)
+    room_score5 = models.IntegerField(blank=True, null=True, choices=SCORE_CHOICES)
 
     def __str__(self):
         return self.title
